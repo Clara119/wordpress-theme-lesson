e5d9054497122afca91a428970f1c5ba0e49a8e7
Clara <whoisclara@gmail.com>
2021-01-26 13:32:26 +0900

    カスタム投稿のスタイルを修正

---
+++ single-daily.php
@@ -0,0 +1,24 @@
+<?php 
+/* 
+Template Name: 日報
+Template Post Type: post, page, event
+*/ 
+?>
+<?php get_header(); 
+the_post();
+?>
+
+<div class="main-content">
+    <div class="daily">
+        <img class="daily__img" src="<?php the_field('img'); ?>" alt="">
+        <h2>これは日報用のPHPです</h2>
+        <p><<?php the_field('day'); ?>/p>
+        <p><?php the_field('content'); ?></p>  
+    </div>
+   
+</div>
+
+
+
+<?php get_footer(); ?>
+
